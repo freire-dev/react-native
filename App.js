dc1e08e7dd43329f09c8ce3dcdb5e9c596569f9f
luanfvtx <luan.freire@vtex.com.br>
2023-08-01 22:05:42 -0300

    Inicio da seção. Foram inseridas na aplicação a pasta de data e models para iniciar o novo projeto.

--- App.js
+++ App.js
@@ -1,79 +1,13 @@
-import StartGame from "./screens/StartGame";
-import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import GameScreen from "./screens/GameScreen";
-import Colors from "./constants/colors";
-import GameOver from "./screens/GameOver";
-import { useFonts } from "expo-font";
-import AppLoading from "expo-app-loading";
+import { View, Text } from "react-native";
 
 const App = () => {
-  const [userNumber, setUserNumber] = useState();
-  const [gamesIsOver, setGameIsOver] = useState(true);
-
-  const [fontsLoaded] = useFonts({
-    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-  });
-
-  if (!fontsLoaded){
-    return <AppLoading/>
-  }
-
-  const pickedNumberHandler = (pickedNumber) => {
-    setUserNumber(pickedNumber);
-    setGameIsOver(false);
-  };
-
-  const gameOverHandler = () => {
-    setGameIsOver(true);
-  };
-
-  let screen = (
-    <StartGame
-      onPickNumber={pickedNumberHandler}
-      onGameOver={gameOverHandler}
-    />
-  );
-
-  if (userNumber) {
-    screen = <GameScreen number={userNumber} />;
-  }
-
-  if (gamesIsOver && userNumber) {
-    screen = <GameOver />;
-  }
-
   return (
     <>
-      <StatusBar style="light" />
-      <LinearGradient
-        colors={[Colors.primary700, Colors.accent500]}
-        style={styles.rootScreen}
-      >
-        <ImageBackground
-          source={require("./assets/images/background.png")}
-          resizeMode="cover"
-          style={styles.rootScreen}
-          imageStyle={styles.backgroundImage}
-        >
-          <SafeAreaView style={styles.rootScreen}>
-            {/* Coloca uma boa margem (espaçamento) padrão para diferentes telas */}
-            {screen}
-          </SafeAreaView>
-        </ImageBackground>
-      </LinearGradient>
+      <View>
+        <Text>Hello World</Text>
+      </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  rootScreen: { flex: 1 },
-  backgroundImage: {
-    opacity: 0.15,
-  },
-});
 
 export default App;
